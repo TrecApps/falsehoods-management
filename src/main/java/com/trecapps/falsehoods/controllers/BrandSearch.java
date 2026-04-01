f57b1a7e457be91f53615ab0b8be8dd7f5dd88bd
John Jacko <jljacko@outlook.com>
2026-03-31 20:52:49 -0600

    Added basic logic for Retrieving Brand Search Results

--- src/main/java/com/trecapps/falsehoods/controllers/BrandSearch.java
+++ src/main/java/com/trecapps/falsehoods/controllers/BrandSearch.java
@@ -1,7 +1,52 @@
 package com.trecapps.falsehoods.controllers;
 
+import com.trecapps.falsehoods.models.BrandSearchResult;
+import com.trecapps.falsehoods.models.ResourceType;
+import com.trecapps.falsehoods.services.BrandService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
-@RestController
+import java.util.List;
+
+@RestController("/brands")
 public class BrandSearch {
+
+    @Autowired
+    BrandService brandService;
+
+    @GetMapping
+    Mono<ResponseEntity<List<BrandSearchResult>>> getBrandsByQuery(
+            @RequestParam String query,
+            @RequestParam(required = false)ResourceType resourceType,
+            @RequestParam(required = false, defaultValue = "10") int limit,
+            @RequestParam(defaultValue = "false") boolean seekAll
+            ){
+        final int tempLimit = limit;
+        return ReactiveSecurityContextHolder.getContext()
+                .flatMap((SecurityContext context) -> {
+                    boolean hasAccess = false;
+                    // ToDo - read the context to determine if requester can create/edit brands
+
+
+                    // End ToDo
+
+                    int cLimit = tempLimit;
+
+                    if(cLimit > 10) cLimit = 10;
+                    if(cLimit < 1) cLimit = 1;
+
+                    return brandService.searchByBrands(
+                            seekAll && hasAccess,
+                            cLimit,
+                            query,
+                            resourceType
+                            );
+                }).map(ResponseEntity::ok);
+    }
 }
--- src/main/java/com/trecapps/falsehoods/services/BrandService.java
+++ src/main/java/com/trecapps/falsehoods/services/BrandService.java
@@ -1,20 +1,41 @@
 package com.trecapps.falsehoods.services;
 
+import com.trecapps.falsehoods.models.Brand;
+import com.trecapps.falsehoods.models.BrandSearchResult;
+import com.trecapps.falsehoods.models.ResourceType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @Service
 public class BrandService {
 
     MongoRepo mongoRepo;
+    IObjectStorageService storageService;
 
     @Autowired
     BrandService(
-            MongoRepo mongoRepo1
+            MongoRepo mongoRepo1,
+            IObjectStorageService storageService1
     ){
         this.mongoRepo = mongoRepo1;
+        this.storageService = storageService1;
     }
 
 
-    
+    public Mono<List<BrandSearchResult>> searchByBrands(boolean hasAccess, int limit, String query, ResourceType resourceType) {
+        return this.mongoRepo.findBrands(query, hasAccess, resourceType, limit)
+                .flatMap((Brand brand) ->
+                    storageService.retrieveThumbnail(brand.getId())
+                            .map((String thumbnail) -> {
+                                BrandSearchResult result = new BrandSearchResult();
+                                result.setBrand(brand);
+                                result.setImage(thumbnail);
+                                return result;
+                            })
+                ).collectList();
+    }
+
 }
